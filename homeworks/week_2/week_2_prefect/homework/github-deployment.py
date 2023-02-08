2a0ae5271d75124d1f10603bc4a599367f4d75a5
Maksim Smirnov <smirnov.maxim@beresnev.games>
2023-02-08 18:13:30 +0400

    week_ 2 -- github deployment

---
+++ homeworks/week_2/week_2_prefect/homework/github-deployment.py
@@ -0,0 +1,16 @@
+from prefect.deployments import Deployment
+from etl_web_to_gcs import etl_web_to_gcs
+from prefect.filesystems import GitHub
+
+storage = GitHub.load("vulnerrags-github")
+
+deployment = Deployment.build_from_flow(
+     flow=etl_web_to_gcs,
+     name="github-example",
+     storage=storage,
+     entrypoint="homeworks/week_2/week_2_prefect/homework/etl_web_to_gcs.py:etl_web_to_gcs",
+     parameters={"color": "green", "year": 2020, "month": 11}
+)
+
+if __name__ == "__main__":
+    deployment.apply()
