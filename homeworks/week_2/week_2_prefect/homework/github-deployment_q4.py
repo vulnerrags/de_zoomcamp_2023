d9a96df64d2d6c566347a5662cea80fec0436aa5
Maksim Smirnov <smirnov.maxim@beresnev.games>
2023-02-08 18:31:44 +0400

    week_ 2 -- q2, q3, q4 fixed

--- homeworks/week_2/week_2_prefect/homework/github-deployment_q4.py
+++ homeworks/week_2/week_2_prefect/homework/github-deployment_q4.py
@@ -1,11 +1,11 @@
 from prefect.deployments import Deployment
-from etl_web_to_gcs import etl_web_to_gcs
+from etl_web_to_gcs_q4 import check_size
 from prefect.filesystems import GitHub
 
 storage = GitHub.load("vulnerrags-github")
 
 deployment = Deployment.build_from_flow(
-     flow=etl_web_to_gcs,
+     flow=check_size,
      name="github-example",
      storage=storage,
      entrypoint="homeworks/week_2/week_2_prefect/homework/etl_web_to_gcs_q4.py:check_size",
